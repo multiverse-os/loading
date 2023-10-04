283e0769bf67b7f9bc894862f4d6ad7bc4a4af9e
Your Name Here <you@example.com>
2023-10-04 04:45:24 -0700

    this is the 5th or so time i have done ansi writing, and this is by far the smallest and most simple version I have came up with'

--- palette.go
+++ palette.go
@@ -6,20 +6,23 @@ import (
 
 // /////////////////////////////////////////////////////////////////////////////
 // Attribute defines a single SGR Code
-type sgr int
+type sgr uint8
+
+func (s sgr) String() string { return fmt.Sprintf("%v", s) }
 
 // Base attributes
 const (
-	Reset sgr = iota
-	Bold
-	Faint
-	Italic
-	Underline
-	BlinkSlow
-	BlinkRapid
-	ReverseVideo
-	Concealed
-	CrossedOut
+	//Reset     sgr = 0
+	Bold      sgr = 1
+	Faint     sgr = 2
+	Italic    sgr = 3
+	Underline sgr = 4
+	Blink     sgr = 5
+	//BlinkRapid sgr = 6
+	Reverse sgr = 7
+	//Concealed sgr = 8
+	CrossedOut sgr = 9
+	Overline   sgr = 53
 )
 
 const (
@@ -45,26 +48,75 @@ const (
 	//HiBackground sgr = 100
 )
 
-// /////////////////////////////////////////////////////////////////////////////
-// NOTE
-// This enables us to do, and it works!
-//
-//	ANSI(Black, Bright, Background) => 0+60+40=100
-func ANSI(attrs ...sgr) (sum sgr) {
+// ////////////////////////////////////////////////////////////////////////////
+// ANSI(Black, Bright, Background) => 0+60+40=100
+func attributes(attrs ...sgr) (sum sgr) {
 	for _, attr := range attrs {
 		sum += attr
 	}
 	return sum
 }
 
-func Color(cName, cType sgr) sgr {
-	return cName + cType
+func color(cName, cType sgr) sgr { return cName + cType }
+
+///////////////////////////////////////////////////////////////////////////////
+// NOTE
+// This enables us to do, and it works!
+
+type text struct {
+	string
+	profile profile
+	attrs   []sgr
 }
 
-func (attr sgr) Text(text string) string {
-	return fmt.Sprintf("%v%s%v", attr, text, attr)
+func (txt text) ansi() (str string) {
+	for _, attr := range txt.attrs {
+		str += attr.String() + ";"
+	}
+	return str
+}
+
+func (txt text) attribute(attr sgr) text {
+	txt.attrs = append(txt.attrs, attr)
+	return txt
+}
+
+func (txt text) attributes(attrs ...sgr) text {
+	for _, attr := range txt.attrs {
+		txt.attribute(attr)
+	}
+	return txt
+}
+
+func (txt text) String() string {
+	if txt.profile == ASCII || len(txt.attrs) == 0 {
+		return txt.string
+	}
+	// TODO: This uses universal reset we want specific reset
+	return fmt.Sprintf("%s%sm%s%sm", CSI, txt.ansi(), txt.string, CSI+RESET)
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 
-const escape = "\x1b"
+type profile uint8
+
+const (
+	ASCII profile = iota
+	ANSI
+)
+
+const (
+	RESET = "0"
+	// Escape character
+	ESC = "\x1b"
+	// Bell
+	BEL = "\a"
+	// Control Sequence Introducer
+	CSI = ESC + "["
+	// Operating System Command
+	OSC = ESC + "]"
+	// String Terminator
+	ST = ESC + `\`
+)
+
+///////////////////////////////////////////////////////////////////////////////
